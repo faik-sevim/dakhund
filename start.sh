665a254c8980fddf2067a8aca3c53e0dbd71c687
Faik <35146324+faik-sevim@users.noreply.github.com>
2023-11-01 19:51:37 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -29,18 +29,41 @@ python3 -c "import flask" >/dev/null 2>&1 || {
     sudo python3 -m pip install Flask
 }
 
-# Step 3: Switch WLAN Device to Monitor Mode
-echo "🔧 Switching WLAN device to monitor mode..."
-sudo ifconfig wlan0 down
-sudo iwconfig wlan0 mode monitor
+# Step 3: Check available WLAN devices and switch the chosen one to Monitor Mode
+echo "🔧 Checking available WLAN devices..."
+
+# List available WLAN devices
+wlan_devices=$(iwconfig 2>&1 | grep 'IEEE 802.11' | awk '{print $1}')
+
+# Convert to array
+wlan_array=($wlan_devices)
+
+# Check number of available WLAN devices
+num_devices=${#wlan_array[@]}
+
+# If there's more than 1 device, prompt user to select one
+selected_device=""
+if [ $num_devices -gt 1 ]; then
+    echo "Multiple WLAN devices found. Please select one:"
+    select opt in "${wlan_array[@]}"; do
+        selected_device=$opt
+        break
+    done
+else
+    selected_device=${wlan_array[0]}
+fi
+
+echo "🔧 Switching $selected_device to monitor mode..."
+sudo ifconfig $selected_device down
+sudo iwconfig $selected_device mode monitor
 
 if [ $? -ne 0 ]; then
-    echo "Unable to switch WLAN to monitor mode. Please ensure you have a WLAN device that supports monitor mode."
+    echo "Unable to switch $selected_device to monitor mode. Please ensure you have a WLAN device that supports monitor mode."
     exit 1
 fi
 
-sudo ifconfig wlan0 up
-echo "✅ WLAN device switched to monitor mode successfully!"
+sudo ifconfig $selected_device up
+echo "✅ $selected_device switched to monitor mode successfully!"
 
 # Step 4: Launch the Program
 echo "🚀 Launching the program..."
